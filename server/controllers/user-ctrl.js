43469abde15d66e50667c891285541ffb6f13372
Moon Yun-gi <yg3988@gmail.com>
2020-09-23 21:41:51 +0900

    09-23

---
+++ server/controllers/user-ctrl.js
@@ -0,0 +1,41 @@
+const User = require('../models/user-models');
+
+createUser = (req, res) => {
+  const body = req.body;
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'Failed to search Character',
+    })
+  }
+
+  const user = new User(body)
+
+  if (!user) {
+    return res.status(400).json({
+      success: false,
+      error: err
+    })
+  }
+
+  user
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: movie._id,
+        message: 'Character Added',
+      })
+    })
+    .catch(error => {
+      return res.status(400).json({
+        error,
+        message: 'Failed to add Character'
+      })
+    })
+}
+
+module.exports = {
+  createUser
+}
